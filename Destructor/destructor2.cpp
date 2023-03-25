464d2fa27da498fbc4da707335caba7b4948ea98
Chandan Kumar <85821143+ChandanArya20@users.noreply.github.com>
2023-03-25 13:15:59 +0530

    Add files via upload

---
+++ Destructor/destructor2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+    public:
+    Base()
+    {
+        cout<<"Base constructor"<<endl;
+    }
+    virtual ~Base()
+    {
+        cout<<"Base destructor"<<endl;
+    }
+};
+class Derived:public Base
+{
+    public:
+    Derived()
+    {
+        cout<<"Derived constructor"<<endl;
+    }
+    ~Derived()
+    {
+        cout<<"Derived destructor"<<endl;
+    }
+};
+int main()
+{
+    // Derived der;
+    Base *p=new Derived;
+    delete p;
+    
+    return 0;
+}
